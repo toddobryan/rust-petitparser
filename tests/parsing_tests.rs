259f8b1bf84e8db318dfe0607a6554f8fd9a77a6
Todd O'Bryan <toddobryan@gmail.com>
2026-05-28 23:43:25 -0400

    Added tests for all matches

--- tests/parsing_tests.rs
+++ tests/parsing_tests.rs
@@ -1,3 +1,4 @@
+use std::rc::Rc;
 use rust_petitparser::parser::ext::ParserExt;
 use googletest::prelude::*;
 use rust_petitparser::core::parser::Parser;
@@ -200,4 +201,45 @@ fn token_test() {
             3
         ))
     );
+}
+
+#[gtest]
+fn all_matches_basic() {
+    let buffer: Rc<[char]> = "abacada".chars().collect::<Vec<_>>().into();
+    let matches: Vec<char> = char('a').all_matches(buffer, 0, false).into_iter().collect();
+    assert_that!(matches, eq(&vec!['a', 'a', 'a', 'a']));
+}
+
+#[gtest]
+fn all_matches_none() {
+    let buffer: Rc<[char]> = "bcdef".chars().collect::<Vec<_>>().into();
+    let matches: Vec<char> = char('a').all_matches(buffer, 0, false).into_iter().collect();
+    assert_that!(matches, eq(&vec![]));
+}
+
+#[gtest]
+fn all_matches_start_position() {
+    let buffer: Rc<[char]> = "aaba".chars().collect::<Vec<_>>().into();
+    let matches: Vec<char> = char('a').all_matches(buffer, 2, false).into_iter().collect();
+    assert_that!(matches, eq(&vec!['a']));
+}
+
+#[gtest]
+fn all_matches_non_overlapping() {
+    let buffer: Rc<[char]> = "aaaa".chars().collect::<Vec<_>>().into();
+    let matches: Vec<(char, char)> = seq2(char('a'), char('a'))
+        .all_matches(buffer, 0, false)
+        .into_iter()
+        .collect();
+    assert_that!(matches, eq(&vec![('a', 'a'), ('a', 'a')]));
+}
+
+#[gtest]
+fn all_matches_overlapping() {
+    let buffer: Rc<[char]> = "aaaa".chars().collect::<Vec<_>>().into();
+    let matches: Vec<(char, char)> = seq2(char('a'), char('a'))
+        .all_matches(buffer, 0, true)
+        .into_iter()
+        .collect();
+    assert_that!(matches, eq(&vec![('a', 'a'), ('a', 'a'), ('a', 'a')]));
 }
